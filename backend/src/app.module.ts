0832d1fa47898c95729b1c56668d9f2d2183e3cf
JeovaniPB <regirabanes@gmail.com>
2026-05-06 14:57:05 -0600

    create: bitacora

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { BitacoraModule } from './bitacora/bitacora.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    BitacoraModule,
   ],
   controllers: [AppController],
   providers: [AppService],
