d6c8d728b2c6e69c6612d26613c6bda3fbce4051
Juan <jemmanuelbn2@gmail.com>
2026-05-06 18:55:00 -0600

    Correccion de pruebas

--- backend/src/reportes/reportes.controller.spec.ts
+++ backend/src/reportes/reportes.controller.spec.ts
@@ -1,14 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportesController } from './reportes.controller';
 import { ReportesService } from './reportes.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ReportesController', () => {
   let controller: ReportesController;
 
   beforeEach(async () => {
+    
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportesController],
-      providers: [ReportesService],
+      providers: [
+            ReportesService,
+            {
+              provide: PrismaService,
+              useValue: {},
+            },
+          ],
     }).compile();
 
     controller = module.get<ReportesController>(ReportesController);
