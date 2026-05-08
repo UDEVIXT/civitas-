dc684703e24b2b49b765c7c0e134a28ba05208f7
Joan Garfias <joanpagarf09@gmail.com>
2026-05-08 00:29:27 -0600

    feat: validation in empleado endpoint

--- backend/src/empleado/empleado.service.ts
+++ backend/src/empleado/empleado.service.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+  BadRequestException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -39,8 +44,6 @@ export class EmpleadoService {
     }
 
     if (byViviendaId) {
-      //Debería obtener la info de la vivienda, solo su campo "numero_vivienda",
-      //el objetivo es obtener el id_residente y con esto hacer un filtro de id_residente a la tabla "Visitante", la cual tiene como FK id_residente
       where.residente = {
         id_vivienda: byViviendaId,
       };
@@ -48,6 +51,8 @@ export class EmpleadoService {
 
     if (isActive !== undefined) {
       where.servicio.activo = isActive;
+    } else {
+      where.servicio.activo = true;
     }
 
     const [data, total] = await Promise.all([
@@ -85,12 +90,84 @@ export class EmpleadoService {
       this.prisma.visitante.count({ where }),
     ]);
 
-    return { data, total };
+    return {
+      meta: {
+        total,
+        page,
+        limit,
+        totalPages: limit ? Math.ceil(total / limit) : 1,
+      },
+      data,
+    };
   }
 
   async eliminarEmpleado(id: string) {
-    await this.prisma.visitante.delete({
-      where: { id_visitante: id },
-    });
+    try {
+      const visitante = await this.prisma.visitante.findUnique({
+        where: {
+          id_visitante: id,
+        },
+        select: {
+          nombre: true,
+          id_servicio: true,
+        },
+      });
+
+      // Error 404
+      if (!visitante) {
+        throw new NotFoundException(
+          `No se encontró ningún empleado con el ID proporcionado.`,
+        );
+      }
+
+      // Error 400
+      if (!visitante.id_servicio) {
+        throw new BadRequestException(
+          `El registro de ${visitante.nombre} no está configurado como un empleado de servicio.`,
+        );
+      }
+
+      //Verificar que el servicio asociado al empleado esté activo antes de intentar darlo de baja
+      const servicio = await this.prisma.servicio.findUnique({
+        where: {
+          id_servicio: visitante.id_servicio,
+          activo: true,
+        },
+        select: {
+          activo: true,
+        },
+      });
+
+      if (!servicio) {
+        throw new NotFoundException(
+          `El empleado que deseas dar de baja no está registrado.`,
+        );
+      }
+      console.log('Servicio encontrado:', servicio);
+
+      // Borrado lógico
+      await this.prisma.servicio.update({
+        where: { id_servicio: visitante.id_servicio },
+        data: { activo: false },
+      });
+
+      return {
+        statusCode: 200,
+        message: `El empleado ${visitante.nombre} ha sido dado de baja exitosamente.`,
+      };
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof BadRequestException
+      ) {
+        throw error;
+      }
+
+      // Error 500: Error inesperado
+      console.error('Error en eliminarEmpleado:', error);
+      throw new InternalServerErrorException(
+        'Ocurrió un error inesperado al intentar dar de baja al empleado. Por favor, inténtelo de nuevo más tarde.',
+      );
+    }
   }
 }
