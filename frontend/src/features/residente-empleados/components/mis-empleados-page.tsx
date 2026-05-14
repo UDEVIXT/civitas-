c90e5706ad7ab0c50ace9e93f83073a1dd7b61eb
Jeremy <jeremyositoxs@gmail.com>
2026-05-13 23:46:01 -0600

    Aplicando vistas responsive
    En la vista principal del archivo mis-empleados-page.tsx se agregaron
    flex-col sm:flex-row sm:items-center justify-between space-y-2 sm:space-y-0

--- frontend/src/features/residente-empleados/components/mis-empleados-page.tsx
+++ frontend/src/features/residente-empleados/components/mis-empleados-page.tsx
@@ -23,9 +23,9 @@ export default function MisEmpleadosPage() {
 
   return (
     <div className="flex-1 space-y-4 p-4 sm:p-8 pt-6">
-      <div className="flex items-center justify-between space-y-2">
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between space-y-2 sm:space-y-0">
         <div>
-          <h2 className="text-3xl font-bold tracking-tight text-foreground">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
             Mis Empleados Domésticos
           </h2>
           <p className="text-muted-foreground">
