e82d823cc99ce2694856f154e0a23463e593572d
SpartanDRK <jhornegallegos@outlook.com>
2026-05-14 21:34:24 -0600

    cambio de axios

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -9,7 +9,7 @@ const apiClient: AxiosInstance = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
     process.env.REACT_APP_API_URL ||
-    "http://localhost:3002/api/",
+    "http://localhost:3001/api/",
   timeout: 10000,
   withCredentials: true,
   headers: {
