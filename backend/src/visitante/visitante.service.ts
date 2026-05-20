1e15bca0869805babdfc943b9d559a6427fef1d6
Joan Garfias <joanpagarf09@gmail.com>
2026-05-20 10:51:40 -0600

    feat: create visitante endpoint

--- backend/src/visitante/visitante.service.ts
+++ backend/src/visitante/visitante.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   Injectable,
   NotFoundException,
@@ -8,36 +10,64 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CreateVisitanteDto } from './dto/create-visitante.dto';
 import { UpdateVisitanteDto } from './dto/update-visitante.dto';
 import 'multer';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 
 @Injectable()
 export class VisitanteService {
   constructor(private prisma: PrismaService) {}
 
-  create(createVisitanteDto: CreateVisitanteDto, file: Express.Multer.File) {
+  async create(
+    createVisitanteDto: CreateVisitanteDto,
+    id_usuario: string,
+    file?: Express.Multer.File,
+  ) {
     const dataVisitante = createVisitanteDto;
-    /*
-    const visitante = this.prisma.$transaction(async (prisma) => {
+
+    //Obtenemos el ID del residente asociado al usuario que hace la petición
+    const residente = await this.prisma.residente.findFirst({
+      where: { id_usuario: id_usuario },
+      select: { id_residente: true },
+    });
+
+    if (!residente) {
+      throw new NotFoundException('Residente not found for the given user ID');
+    }
+
+    //Validamos si fecha_fin es undefined, entonces será igual a la fecha de inicio (acceso de una sola ocasión)
+    const fechaExpiracion =
+      dataVisitante.fecha_fin || dataVisitante.fecha_inicio;
+
+    //Registramos al visitante con sus datos
+    return await this.prisma.$transaction(async (prisma) => {
       try {
-        const createdVisitante = await prisma.visitante.create({
+        const visitante = await prisma.visitante.create({
           data: {
             nombre: dataVisitante.nombre,
             es_frecuente: dataVisitante.es_frecuente,
-
-            //tipo_visitante: data.tipo_visitante,
             telefono: dataVisitante.telefono,
-            //tipo_vehiculo: data.tipo_vehiculo,
+            tipo_vehiculo: dataVisitante.tipo_vehiculo,
+            motivo: dataVisitante.tipo_visitante,
+            url_imagen: null,
+            residente: {
+              connect: { id_residente: residente.id_residente },
+            },
+            accesos: {
+              create: {
+                id_usuario: id_usuario,
+                estatus: 'Activo',
+                codigo_qr: randomStringGenerator(),
+                fecha_creacion: dataVisitante.fecha_inicio,
+                fecha_expiracion: fechaExpiracion,
+              },
+            },
           },
         });
-        return createdVisitante;
-      } catch (error: any) {
-        throw new InternalServerErrorException(
-          'Error al crear el visitante',
-          error.message,
-        );
+        return visitante;
+      } catch (error) {
+        console.error('Error creating visitante:', error);
+        throw new InternalServerErrorException('Failed to create visitante');
       }
     });
-    */
-    return 'Visitante creado exitosamente';
   }
 
   findAll() {
